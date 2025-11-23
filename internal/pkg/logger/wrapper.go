49f496c1ca99c6f97eff23fb5847e7bbbc4067fc
User <user@Users-MacBook-Pro.local>
2025-11-23 23:51:57 +0700

    feat: Add level router logger for automatic log file routing
    - Create LevelRouterLogger to route logs to appropriate files by level
    - Info logs -> info.log
    - Warning logs -> warning.log
    - Error logs -> error.log
    - Update main.go to use level router logger
    - All existing log calls will automatically route to correct files

---
+++ internal/pkg/logger/wrapper.go
@@ -0,0 +1,94 @@
+package logger
+
+import (
+	"context"
+
+	"github.com/go-kratos/kratos/v2/log"
+)
+
+// LevelRouterLogger routes logs to different files based on level
+type LevelRouterLogger struct {
+	fileLoggers *FileLoggers
+}
+
+// NewLevelRouterLogger creates a logger that routes logs to appropriate files
+func NewLevelRouterLogger(fileLoggers *FileLoggers) log.Logger {
+	return &LevelRouterLogger{
+		fileLoggers: fileLoggers,
+	}
+}
+
+// Log implements log.Logger interface
+func (l *LevelRouterLogger) Log(level log.Level, keyvals ...interface{}) error {
+	switch level {
+	case log.LevelInfo:
+		return l.fileLoggers.InfoLogger.Log(level, keyvals...)
+	case log.LevelWarn:
+		return l.fileLoggers.WarningLogger.Log(level, keyvals...)
+	case log.LevelError:
+		return l.fileLoggers.ErrorLogger.Log(level, keyvals...)
+	default:
+		return l.fileLoggers.StdoutLogger.Log(level, keyvals...)
+	}
+}
+
+// WithContext returns a logger with context
+func (l *LevelRouterLogger) WithContext(ctx context.Context) log.Logger {
+	// Create new loggers with context
+	infoLogger := log.NewHelper(l.fileLoggers.InfoLogger).WithContext(ctx)
+	warnLogger := log.NewHelper(l.fileLoggers.WarningLogger).WithContext(ctx)
+	errorLogger := log.NewHelper(l.fileLoggers.ErrorLogger).WithContext(ctx)
+	stdoutLogger := log.NewHelper(l.fileLoggers.StdoutLogger).WithContext(ctx)
+
+	return &LevelRouterLoggerWithContext{
+		infoHelper:    infoHelper{helper: infoLogger},
+		warnHelper:    warnHelper{helper: warnLogger},
+		errorHelper:   errorHelper{helper: errorLogger},
+		stdoutHelper:  stdoutHelper{helper: stdoutLogger},
+	}
+}
+
+// Helper types for context-aware logging
+type infoHelperType struct {
+	helper *log.Helper
+}
+
+type warnHelperType struct {
+	helper *log.Helper
+}
+
+type errorHelperType struct {
+	helper *log.Helper
+}
+
+type stdoutHelperType struct {
+	helper *log.Helper
+}
+
+// LevelRouterLoggerWithContext is a context-aware logger that routes by level
+type LevelRouterLoggerWithContext struct {
+	infoHelper   infoHelperType
+	warnHelper   warnHelperType
+	errorHelper  errorHelperType
+	stdoutHelper stdoutHelperType
+}
+
+// Log implements log.Logger interface with context
+func (l *LevelRouterLoggerWithContext) Log(level log.Level, keyvals ...interface{}) error {
+	switch level {
+	case log.LevelInfo:
+		return l.infoHelper.helper.Log(level, keyvals...)
+	case log.LevelWarn:
+		return l.warnHelper.helper.Log(level, keyvals...)
+	case log.LevelError:
+		return l.errorHelper.helper.Log(level, keyvals...)
+	default:
+		return l.stdoutHelper.helper.Log(level, keyvals...)
+	}
+}
+
+// WithContext returns itself (already has context)
+func (l *LevelRouterLoggerWithContext) WithContext(ctx context.Context) log.Logger {
+	return l
+}
+
